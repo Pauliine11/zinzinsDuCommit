a1f71096504c54620a45901b1addae51f039072e
Pauliine11 <paulinevires@.com>
2025-09-29 16:34:10 +0200

    creation du router

--- public/index.php
+++ public/index.php
@@ -1,2 +1,10 @@
 <?php
-require_once(__DIR__ . "/../src/Views/partials/head.view.php");
+require_once(__DIR__ . '/../vendor/autoload.php');
+
+use Config\Tata;
+
+$router = new Tata();
+
+$router->addRoute('/', 'HomeController', 'index');
+
+$router->handleRequest();
--- src/Views/partials/head.view.php
+++ src/Views/partials/head.view.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Les zinzins du commit</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="public/assets/css/style.css">
+    <link rel="shortcut icon" href="/public/assets/img/logo.png" type="image/x-icon">
+    <script src="https://kit.fontawesome.com/f5a1d28d53.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="/public/assets/css/style.css">
 </head>
 <body>
     <header>
